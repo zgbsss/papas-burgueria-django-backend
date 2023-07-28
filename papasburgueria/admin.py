efbfe8c69955a97cda505f86cb75bc0660946eb2
zgbsss <gabrielasaraiva.04@gmail.com>
2023-07-28 13:42:03 -0300

    Correção em Models 2

--- papasburgueria/admin.py
+++ papasburgueria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from papasburgueria.models import Hamburguer, Ingrediente, Lucro, Refrigerante
+
+admin.site.register(Hamburguer)
+admin.site.register(Ingrediente)
+admin.site.register(Lucro)
+admin.site.register(Refrigerante)   
+
 
 # Register your models here.
--- papasburgueria/models/hamburguer.py
+++ papasburgueria/models/hamburguer.py
@@ -1,9 +1,7 @@
 from django.db import models
-from papasburgueria.models import Ingrediente
 
 class Hamburguer(models.Model):
     nome = models.CharField(max_length=255)
-    ingrediente = models.ForeignKey (Ingrediente, on_delete=models.PROTECT, related_name="ingredientes")
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank= True)
 
     def __str__(self):
