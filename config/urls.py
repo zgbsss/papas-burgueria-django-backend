fe4b8bb9e6dd4398c6652c87ac6f218436e3d695
zgbsss <gabrielasaraiva.04@gmail.com>
2023-08-01 12:34:00 -0300

    Adição das Funções das URLS

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from papasburgueria.views import HamburguerViewSet, RefrigeranteViewSet, IngredienteViewSet, LucroViewSet
+
+router = DefaultRouter()
+router.register(r"hamburgueres", HamburguerViewSet)
+router.register(r"refrigerantes", RefrigeranteViewSet)
+router.register(r"ingredientes", IngredienteViewSet)
+router.register(r"lucros", LucroViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
-
-# faz as url :) só falta elas :')
