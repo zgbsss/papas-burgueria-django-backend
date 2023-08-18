9e7e098b7cadb495f4fab394fd7c557c5af0b418
zgbsss <gabrielasaraiva.04@gmail.com>
2023-08-18 11:00:19 -0300

    Correção dos Erros

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "rest_framework",
-    "rest_framework_simplejwt"   
     "papasburgueria",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "setuptools",
 ]
 
 MIDDLEWARE = [
