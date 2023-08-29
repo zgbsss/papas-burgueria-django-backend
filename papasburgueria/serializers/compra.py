ce32669bb183240b7866a802aa588ca6c8747da0
zgbsss <gabrielasaraiva.04@gmail.com>
2023-08-29 09:19:33 -0300

    Finalização dos Endpoints para Compra

--- papasburgueria/serializers/compra.py
+++ papasburgueria/serializers/compra.py
@@ -2,6 +2,8 @@ from rest_framework.serializers import ModelSerializer, CharField
 from papasburgueria.models import Compra
 
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source="usuario.email", read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
