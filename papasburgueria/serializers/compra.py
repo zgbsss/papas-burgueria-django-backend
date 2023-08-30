dafb4563f198b5e6c0f17d3f6eb52aec2347406f
zgbsss <gabrielasaraiva.04@gmail.com>
2023-08-30 16:19:37 -0300

    Endpoint do ItensCompra

--- papasburgueria/serializers/compra.py
+++ papasburgueria/serializers/compra.py
@@ -1,9 +1,17 @@
 from rest_framework.serializers import ModelSerializer, CharField
-from papasburgueria.models import Compra
+from papasburgueria.models import Compra, ItensCompra
+
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ["hamburguer", "quantidade"]
+        depth = 2
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     status = CharField(source="get_status_display", read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
         fields = "__all__"
