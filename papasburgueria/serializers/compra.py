a68903bd7a1000156f669e2fefd7fa9df56b8712
zgbsss <gabrielasaraiva.04@gmail.com>
2023-08-30 16:55:36 -0300

    Continuação do Endpoint de ItensCompra

--- papasburgueria/serializers/compra.py
+++ papasburgueria/serializers/compra.py
@@ -1,12 +1,14 @@
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 from papasburgueria.models import Compra, ItensCompra
 
-
 class ItensCompraSerializer(ModelSerializer):
+    total = SerializerMethodField()
     class Meta:
         model = ItensCompra
         fields = ["hamburguer", "quantidade"]
         depth = 2
+    def get_total(self, obj):
+        return obj.hamburguer.preco * obj.quantidade
 
 class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
