936f1697a1323714e7f9b24381320059b73c132a
zgbsss <gabrielasaraiva.04@gmail.com>
2023-11-28 09:29:43 -0300

    Inicio de foto de perfil

--- usuario/models.py
+++ usuario/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uploader.models import Image
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
@@ -9,6 +10,7 @@ class Usuario(AbstractUser):
     username = None
     email = models.EmailField(_("e-mail address"), unique=True)
     cpf = models.CharField(_("CPF"), max_length=11, blank=True, null=True)
+    foto = models.ForeignKey (Image, on_delete=models.SET_NULL, null=True, blank=True, default=None)
     telefone = models.CharField(_("Phone"), max_length=11, blank=True, null=True)
     data_nascimento = models.DateField(
         _("Birth Date"), auto_now=False, auto_now_add=False, blank=True, null=True
