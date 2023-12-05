3f6a09f44a382716ac5fa718162d137fff00f7e5
zgbsss <gabrielasaraiva.04@gmail.com>
2023-12-05 15:37:11 -0300

    Correção de erro na URL

--- config/urls.py
+++ config/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     path("api/usuarios", include(usuario_router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/media", include(uploader_router.urls)),
+    path("api/media/", include(uploader_router.urls)),
     # OpenAPI
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
