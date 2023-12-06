d85c35788feb7d3145b81e940a54c488f6275961
zgbsss <gabrielasaraiva.04@gmail.com>
2023-12-06 16:10:44 -0300

    Correção do Erro com Imagem em Hamburguer

--- papasburgueria/models/hamburguer.py
+++ papasburgueria/models/hamburguer.py
@@ -7,7 +7,7 @@ class Hamburguer(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank= True)
     ingredientes = models.ManyToManyField(Ingrediente, related_name="hamburguer")
     avaliacao = models.DecimalField(max_digits=5, decimal_places= 0, default=0, null= True, blank= True)
-    image = models.ForeignKey(Image, related_name="+", on_delete=models.CASCADE, null=True, blank=True, default=None)
+    imagem = models.ForeignKey(Image, related_name="+", on_delete=models.CASCADE, null=True, blank=True, default=None)
 
     def __str__(self):
         return f"{self.nome} - {self.preco}"    
--- papasburgueria/serializers/hamburguer.py
+++ papasburgueria/serializers/hamburguer.py
@@ -1,18 +1,31 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from papasburgueria.models import Hamburguer
+from uploader.serializers import ImageSerializer
+from uploader.models import Image
 
 class HamburguerSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="imagem",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    imagem = ImageSerializer(required=False, read_only=True)
     class Meta:
         model = Hamburguer
         fields = "__all__"
 
 class HamburguerDetailSerializer(ModelSerializer):
+    imagem = ImageSerializer(required=False)
     class Meta:
         model = Hamburguer
         fields = "__all__"
         depth = 1
 
 class HamburguerListSerializer(ModelSerializer):
+    imagem = ImageSerializer(required=False)
     class Meta:
         model = Hamburguer
-        fields = ["id", "nome", "preco", "ingredientes", "avaliacao"]
+        fields = ["id", "imagem", "nome", "preco", "ingredientes", "avaliacao"]
+        depth = 1
